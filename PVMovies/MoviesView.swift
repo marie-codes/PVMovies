ca22c37de5983ea612f18f47936491eabdf8b665
Marie Park <MariePark@Maries-Air-2.home>
2017-07-12 11:22:18 -0400

    Set up project structure

---
+++ PVMovies/MoviesView.swift
@@ -0,0 +1,21 @@
+//
+//  MoviesView.swift
+//  PVMovies
+//
+//  Created by Marie Park on 7/12/17.
+//  Copyright © 2017 Marie Park. All rights reserved.
+//
+
+import UIKit
+
+class MoviesView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
